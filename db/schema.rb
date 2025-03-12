bdf949c60e2871a85e9cd11789c1931aeabbc4c4
jayandra <jayandra@umn.edu>
2025-03-12 17:39:16 -0500

    rails g scaffold Expense amount:float description:text category:belongs_to

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_12_174231) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_12_223821) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "parent_id"
@@ -19,5 +19,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_12_174231) do
     t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
 
+  create_table "expenses", force: :cascade do |t|
+    t.float "amount"
+    t.text "description"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_expenses_on_category_id"
+  end
+
   add_foreign_key "categories", "categories", column: "parent_id"
+  add_foreign_key "expenses", "categories"
 end
