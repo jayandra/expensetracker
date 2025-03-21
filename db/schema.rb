d28c865a9ab9b2dadcaecb921c7e514df3bc57d4
jayandra <jayandra@umn.edu>
2025-03-21 12:58:35 -0500

    bin/rails generate authentication followed by bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_18_040438) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_21_175125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -32,6 +32,24 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_18_040438) do
     t.index ["category_id"], name: "index_expenses_on_category_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "categories", "categories", column: "parent_id"
   add_foreign_key "expenses", "categories"
+  add_foreign_key "sessions", "users"
 end
