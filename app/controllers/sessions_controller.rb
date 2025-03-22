840cf58256e515d834dc967ce0cbce543e086b80
jayandra <jayandra@umn.edu>
2025-03-21 22:40:03 -0500

    Added signup and moved error display block to its own partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,46 @@
 class SessionsController < ApplicationController
-  allow_unauthenticated_access only: %i[ new create ]
+  allow_unauthenticated_access only: %i[ new create signup complete_signup]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
   end
 
   def create
-    if user = User.authenticate_by(params.permit(:email_address, :password))
-      start_new_session_for user
+    @user = User.authenticate_by(session_params)
+    if @user
+      start_new_session_for @user
       redirect_to after_authentication_url
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      @errors = User.new.errors
+      @errors.add(:base, "Try another email address or password")
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     terminate_session
     redirect_to new_session_path
+  end
+
+
+  def signup
+    render :signup
+  end
+
+  def complete_signup
+    @user = User.new(session_params)
+    if @user.save
+      start_new_session_for @user
+      Category.seed_category_for_new_user(@user)
+      redirect_to categories_path, notice: "Welcome to ExpenseTracker!"
+    else
+      render :signup, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def session_params
+    params.permit(:email_address, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
   resources :expenses
   resources :categories
+
+  get "signup", to: "sessions#signup"
+  post "complete_signup", to: "sessions#complete_signup"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
