55aa3387eb5bb03c0d43cc6e058754b0cfce0994
jayandra <jayandrapokharel@gmail.com>
2025-08-14 11:30:52 -0500

    minor: got the json response working such that frontend could consume it.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @root_categories = scoped_categories.roots
+    @categories = scoped_categories
   end
 
   # GET /categories/1 or /categories/1.json
